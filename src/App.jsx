5df4290587814e308a89acac7e1f7ace1f8a8ea0
Daviloy <ubakawilson@gmail.com>
2022-05-20 13:34:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const App = () => {
+    return (
+        <>
+            <h1>Vasiti Testimonials</h1>
+        </>
+    );
+};
+
+export default App;
+
+// set up a git repo and create a github repositiory to make your commits
+// install Material UI for the modal.
+// components
+// Header
+//    Nav
+//    Showcase
